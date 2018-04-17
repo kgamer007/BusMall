7fbe5d88b495833856c94ba003cdd1659e2d09f4
Kevin Hwangpo <hwangpo3@gmail.com>
2018-04-17 16:54:49 -0700

    added a random function

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # BusMall
 
-4/16/2018  4:45pm
+4/16/2018 4:45pm
 Got started on setting up my BusMall assignment. 
+
+4/17/2018 2:30pm
+Pulling up Monday's assignment and attempt to finish and catch up to Tuesday's work. 
--- js/app.js
+++ js/app.js
@@ -2,50 +2,77 @@
 
 console.log('js is linked');
 
-var userChoosesButton1 = document.getElementById('User-choice-1');
-var userChoosesButton2 = document.getElementById('User-choice-2');
-var userChoosesButton3 = document.getElementById('User-choice-3');
+AllImages.all = [];
 
-function randomImages(url) {
+var userChoosesButton1 = document.getElementById('user-choice-1');
+var userChoosesButton2 = document.getElementById('user-choice-2');
+var userChoosesButton3 = document.getElementById('user-choice-3');
+var firstPicture = document.getElementById('firstImage');
+var secondPicture = document.getElementById('secondImage');
+var thirdPicture = document.getElementById('thirdImage');
+
+function AllImages(url, name) {
   this.url = url;
+  this.name = name;
   this.votes = 0;
+  AllImages.all.push(this);
 }
+new AllImages('img/bag.jpg', 'bag');
+new AllImages('img/banana.jpg', 'banana');
+new AllImages('img/bathroom.jpg', 'bathroom');
+new AllImages('img/boots.jpg', 'boots');
+new AllImages('img/breakfast.jpg', 'breakfast');
+new AllImages('img/bubblegum.jpg', 'bubblegum');
+new AllImages('img/chair.jpg', 'chair');
+new AllImages('img/cthulhu.jpg', 'cthulhu');
+new AllImages('img/dog-duck.jpg', 'dog-duck');
+new AllImages('img/dragon.jpg', 'dragon');
+new AllImages('img/pen.jpg', 'pen');
+new AllImages('img/pet-sweep.jpg', 'pet-sweep');
+new AllImages('img/scissors.jpg', 'scissors');
+new AllImages('img/shark.jpg', 'shark');
+new AllImages('img/sweep.png', 'sweep');
+new AllImages('img/tauntaun.jpg', 'tauntaun');
+new AllImages('img/unicorn.jpg', 'unicorn');
+new AllImages('img/usb.gif', 'usb');
+new AllImages('img/water-can.jpg', 'water-can');
+new AllImages('img/wine-glass.jpg', 'wine-glass');
 
-var allAnswers = [
-  new randomImages('img/bag.jpg'),
-  new randomImages('img/banana.jpg'),
-  new randomImages('img/bathroom.jpg'),
-  new randomImages('img/boots.jpg'),
-  new randomImages('img/breakfast.jpg'),
-  new randomImages('img/bubblegum.jpg'),
-  new randomImages('img/chair.jpg'),
-  new randomImages('img/cthulhu.jpg'),
-  new randomImages('img/dog-punk.jpg'),
-  new randomImages('img/dragon.jpg'),
-  new randomImages('img/pen.jpg'),
-  new randomImages('img/pet-sweep.jpg'),
-  new randomImages('img/scissors.jpg'),
-  new randomImages('img/shark.jpg'),
-  new randomImages('img/sweep.jpg'),
-  new randomImages('img/tauntaun.jpg'),
-  new randomImages('img/unicorn.jpg'),
-  new randomImages('img/usb.jpg'),
-  new randomImages('img/water-can.jpg'),
-  new randomImages('img/wine-glass.jpg')
-];
+function randomImages() {
+  var index1 = Math.floor(Math.random() * AllImages.all.length);
+  var index2 = Math.floor(Math.random() * AllImages.all.length);
+  var index3 = Math.floor(Math.random() * AllImages.all.length);
 
-var userAnswers1 = allAnswers[0];
-var userAnswers2 = allAnswers[1];
-var userAnswers3 = allAnswers[2];
+  var randomImage = AllImages.all[index1];
+  var randomImage2 = AllImages.all[index2];
+  var randomImage3 = AllImages.all[index3];
 
-userChoosesButton1.addEventListener('click', function(e) {
-  userAnswers1.votes++;
-});
+firstPicture.src = randomImage.url;
+secondPicture.src = randomImage2.url;
+thirdPicture.src = randomImage3.url;
 
-userChoosesButton2.addEventListener('click', function(e) {
-  userAnswers2.votes++;
-});
+}
+randomImages();
 
-userChoosesButton3.addEventListener('click', function(e) {
-  userAnswers3.votes++;
-});
+
+
+
+// userChoosesButton1.addEventListener('click', function (e) {
+//   image1.votes++;
+//   image1 = AllImages[Math.floor(Math.random() * theListOfJPG.length)];
+//   firstPicture.src = image1.url;
+//   image2 = AllImages[Math.floor(Math.random() * theListOfJPG.length)];
+//   secondPicture.src = image2.url;
+// });
+
+// userChoosesButton2.addEventListener('click', function (e) {
+//   image2.votes++;
+//   image2 = AllImages[Math.floor(Math.random() * theListOfJPG.length)];
+//   secondPicture.src = image2.url;
+// });
+
+// userChoosesButton3.addEventListener('click', function (e) {
+//   image3.votes++;
+//   image3 = AllImages[Math.floor(Math.random() * theListOfJPG.length)];
+//   thirdPicture.src = image3.url;
+// });
